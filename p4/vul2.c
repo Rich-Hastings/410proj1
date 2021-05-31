0662c1ccede2077b94a290b3abeae94331492fe4
Rich-Hastings <user@richdev.Home>
2021-05-30 19:33:46 -0700

    stuff

---
+++ p4/vul2.c
@@ -0,0 +1,35 @@
+//
+// Created by user on 5/30/21.
+//
+
+Exercise/indi-p3 (vul2.c)•Create an exploit to•Print out account.note•No segfault is caused
+#include <stdio.h>
+#include <string.h>
+
+void proc_input(char* source) {
+    unsigned char buf[80];
+    strcpy(buf, source);
+}
+
+int main() {
+    struct {
+        int balance;
+        int type;
+        int interest;
+        char note[174];
+    }account;
+    account.balance = 88888888;
+    account.type = 1;
+    account.interest = 1;
+    strncpy(account.note, "AAA", 4);
+
+    unsigned char buf[128] = {'\0'};
+    FILE *fp;
+
+    fp = fopen("abc", "rb");
+    fread(buf, 128, 1, fp);
+
+    proc_input(buf);
+
+    fclose(fp);
+}
